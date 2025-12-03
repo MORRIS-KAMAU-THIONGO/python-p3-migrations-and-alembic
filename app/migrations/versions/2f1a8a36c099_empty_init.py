8f592bf2f8a427140fbb90c93f072d8177afa37c
M.K.Thiong'o <kamauthiongo58@gmail.com>
2025-12-03 13:51:42 +0300

    update migration and alchemy lab

---
+++ app/migrations/versions/2f1a8a36c099_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 2f1a8a36c099
+Revises: 
+Create Date: 2025-12-03 13:12:40.616308
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2f1a8a36c099'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
